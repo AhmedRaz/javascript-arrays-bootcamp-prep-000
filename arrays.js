4272881d80358ab5e9be382bbfc423b3e105f8de
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-04-17 05:13:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,1 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']; //since when are skittles considered chocolate bars?
